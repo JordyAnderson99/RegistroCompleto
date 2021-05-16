7db732d74b65dfb501889a5cd9b2836aaac57669
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-05-15 23:43:08 -0400

    Add project files.

---
+++ Dal/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistroCompleto.Models;
+
+namespace RegistroCompleto.Dal
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data/Personas.db");
+        }
+    }
+}
